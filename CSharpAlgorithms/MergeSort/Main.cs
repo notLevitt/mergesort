f8b8c3dd8feaaeaae8608f89935c1cefbdd7fed1
notLevitt <117864412+notLevitt@users.noreply.github.com>
2022-11-10 12:38:11 -0700

    mergesort

---
+++ CSharpAlgorithms/MergeSort/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace testconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {1,2,3 };
+            Merge.mergeSort(array);
+        }   
+    }
+}
